eb8da5842a4341176e53afb83d3195b6b0b57079
Jeremy Muñoz Torres <JMunoz@contractor.nexstardigital.com>
2021-10-06 08:41:47 -0400

    Added Main React App

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './index.scss';
+
+interface TweetsData {
+  data: {
+    end: string;
+    start: string;
+    tweet_count: number;
+  }[];
+
+  meta: {
+    total_tweet_count: number;
+  };
 }
 
-export default App;
+const GROUPS = ['light', 'medium', 'full', 'full'];
+
+const getModifier = (max: number, min: number, count: number) => {
+  const group = Math.round(((count - min) * 3) / (max - min));
+
+  if (count === 0) return 'none';
+  return GROUPS[group];
+};
+
+const getMaxAndMinCount = (tweetsData: TweetsData) => {
+  let min = Number.POSITIVE_INFINITY;
+  let max = Number.NEGATIVE_INFINITY;
+  let tmp: number;
+
+  for (let i = tweetsData.data.length - 1; i >= 0; i--) {
+    tmp = tweetsData.data[i].tweet_count;
+    if (tmp < min) min = tmp;
+    if (tmp > max) max = tmp;
+  }
+
+  return { min, max };
+};
+
+const TweetsGraph = () => {
+  const [data, setData] = useState<TweetsData | null>(null);
+
+  return (
+    <section className="tweets-container">
+      <div className="tweets-header">
+        <a className="profile-link">{'@<username>'}</a>
+      </div>
+
+      <div className="tweets-graph">
+        {data?.data.map((item) => {
+          const KEY = `${item.start}-${item.end}`;
+          const modifier = getModifier(max, min, item.tweet_count);
+
+          return <span key={KEY} className={`tweets-graph-item ${modifier}`} />;
+        })}
+      </div>
+
+      <div className="tweets-footer">Download icobn</div>
+    </section>
+  );
+};
+
+export default TweetsGraph;
